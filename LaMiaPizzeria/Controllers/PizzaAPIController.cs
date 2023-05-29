a1169a2e730b7991ce44e6c7346841e7970c0f3b
Khodazone <khodazone@gmail.com>
2023-05-29 17:41:15 +0200

    action

--- LaMiaPizzeria/Controllers/PizzaAPIController.cs
+++ LaMiaPizzeria/Controllers/PizzaAPIController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LaMiaPizzeria.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class PizzaAPIController : ControllerBase
     {
