a6fcebb8cf3cd5c2ede959a1efcc631b50ac9de4
Khodazone <khodazone@gmail.com>
2023-05-29 17:27:30 +0200

    get

--- LaMiaPizzeria/Controllers/PizzaAPIController.cs
+++ LaMiaPizzeria/Controllers/PizzaAPIController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using csharp_ef_pizze;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LaMiaPizzeria.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class PizzaAPIController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetPizza()
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> list = db.Pizza.ToList();
+                return Ok(list);
+            }
+        }
     }
 }
